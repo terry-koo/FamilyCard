5d29c1f92766220fbb5c8fa1a511d2e5f2546eb1
Terry-Koo <xpflxhfl94@naver.com>
2022-10-08 16:39:01 +0900

    init

---
+++ FamilyCard/FamilyCardApp.swift
@@ -0,0 +1,20 @@
+//
+//  FamilyCardApp.swift
+//  FamilyCard
+//
+//  Created by Terry Koo on 2022/10/08.
+//
+
+import SwiftUI
+
+@main
+struct FamilyCardApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
