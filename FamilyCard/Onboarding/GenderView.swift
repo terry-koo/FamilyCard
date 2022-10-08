f69940cba676728f892bf4685e7a4888341e6040
Terry-Koo <xpflxhfl94@naver.com>
2022-10-08 18:25:12 +0900

    [feat] 온보딩 / 이름 입력 뷰 구현

---
+++ FamilyCard/Onboarding/GenderView.swift
@@ -0,0 +1,31 @@
+//
+//  GenderView.swift
+//  FamilyCard
+//
+//  Created by Terry Koo on 2022/10/08.
+//
+
+import SwiftUI
+
+struct GenderView: View {
+    @State var username: String = "린다"
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("성별을 선택해주세요")
+                .font(.system(size: 28))
+                .padding(.vertical, 40)
+            TextField("이름", text: $username)
+            Divider()
+                .frame(height: 1)
+                .background(Color.blue)
+            Spacer()
+        }
+        .padding(.horizontal, 30)
+    }
+}
+
+struct GenderView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenderView()
+    }
+}
---
+++ FamilyCard/Onboarding/NameView.swift
@@ -0,0 +1,66 @@
+//
+//  NameView.swift
+//  FamilyCard
+//
+//  Created by Terry Koo on 2022/10/08.
+//
+
+import SwiftUI
+
+struct NameView: View {
+    enum FocusField: Hashable {
+      case field
+    }
+    @State private var username: String = ""
+    @FocusState private var focusedField: FocusField?
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("이름을 입력해주세요")
+                .font(.system(size: 28))
+                .padding(.vertical, 40)
+            HStack {
+                TextField("이름", text: $username)
+                    .focused($focusedField, equals: .field)
+                    .keyboardType(.twitter)
+                    .onAppear {
+                        self.focusedField = .field
+                    }
+                if !username.isEmpty {
+                    Button(action: {
+                        username = ""
+                    }, label: {
+                        Image(systemName: "x.circle.fill")
+                            .foregroundColor(.gray)
+                    })
+                }
+            }
+            Divider()
+                .frame(height: 1)
+                .background(Color.blue)
+            Spacer()
+        }
+        .padding(.horizontal, 30)
+        
+        // 다음 버튼
+        VStack {
+            if focusedField == .field && !username.isEmpty {
+                Button(action: {
+                    print("button")
+                }, label: {
+                    Text("다음")
+                        .padding(10)
+                        .frame(width: UIScreen.main.bounds.size.width, height: 60)
+                        .foregroundColor(.white)
+                        .background(.blue)
+                })
+            }
+        }
+    }
+}
+
+struct NameView_Previews: PreviewProvider {
+    static var previews: some View {
+        NameView() 
+    }
+}
